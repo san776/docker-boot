fb6018d6af544a541ef516ca92711022caa13929
sandeep kumar <75674516+san776@users.noreply.github.com>
2025-01-29 22:14:38 +0530

    hello

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Welcome sandeep is here  learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 18th, 2025");
+        model.addAttribute("title", " hello Welcome sandeep is here  learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 18th, 2025");
         model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
     }
